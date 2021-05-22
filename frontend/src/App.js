742a5d5b1351389569dd2f3f7db17509b9064e49
Joseph Duron Jr <djesph@gmail.com>
2021-05-22 12:55:25 +0800

    [Feature][Joseph] added LeftContainer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import LeftContainer from './components/LeftContainer';
 function App() {
   return (
     <div className="App">
-      <Grid/>
+      {/*<Grid/>*/}
       <LeftContainer/>
     </div>
   );
--- frontend/src/components/LeftContainer.js
+++ frontend/src/components/LeftContainer.js
@@ -1,10 +1,20 @@
 import React, { useState } from 'react'
-import {Button} from 'bootstrap'
-
+import '../css/LeftContainer.css'
 
 const LeftContainer = () => {
-    <Button>Test</Button>
-
+    return(
+        <div class=".container border bg-black" id="left-container">
+            <div class=" row justify-content-center align-items-center" id="new-pixel-art">
+            <button type="button" class="col-sm-5 btn btn-primary">New Pixel Art</button>
+            </div>
+            <div class="row justify-content-center align-items-center" id="test">
+            <button type="button" class="col-sm-5 btn btn-primary">test</button>
+            </div>
+            <div class="row justify-content-center align-items-center" id="save-pixel-art">
+            <button type="button" class="col-sm-5 btn btn-danger">Save Pixel Art</button>
+            </div>
+        </div>    
+    )
 }
 
 export default LeftContainer;
