3e994b4218c84494597c738e6a8c0c2d3b910e6b
Sheen <sheen@symph.co>
2021-05-22 18:33:20 +0800

    [Adjustment][Sheen] Removed time travel

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { initialGridState } from './helpers/Grid';
 import { PAINT } from './constants/ActionTypes';
-import { GRID_HISTORY_LIMIT } from './constants/Grid';
 
 import Grid from './components/Grid';
 import PaintOptions from './components/PaintOptions';
@@ -9,18 +8,9 @@ import LeftContainer from './components/LeftContainer';
 
 function App() {
 
-  const [gridHistory, setGridHistory] = useState([]);
   const [gridState, setGridState] = useState(initialGridState);
   const [actionType, setActionType] = useState(PAINT);
   const [color, setColor] = useState();
-
-  useEffect(() => {
-    const newGridHistory = [ ...gridState, gridState ];
-    if (newGridHistory.length > GRID_HISTORY_LIMIT) {
-      newGridHistory.shift();
-    }
-    setGridHistory(newGridHistory);
-  }, [gridState]);
 
   return (
     <div className="App container">
